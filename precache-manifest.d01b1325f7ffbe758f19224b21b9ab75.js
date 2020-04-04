6515653e6e04a88a331c26232d940ea70c0f3dfd
xoma_star <40947642+xoma-star@users.noreply.github.com>
2020-04-04 09:58:42 +0500

    Updates

---
+++ precache-manifest.d01b1325f7ffbe758f19224b21b9ab75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b2e91a64d014b68be3320fa43562a3",
+    "url": "/vk_rps/index.html"
+  },
+  {
+    "revision": "29e81cbf049f0d6eb0c5",
+    "url": "/vk_rps/static/css/2.4fc5a6bc.chunk.css"
+  },
+  {
+    "revision": "9bbbfdbf292ea580c6a6",
+    "url": "/vk_rps/static/css/main.8447535d.chunk.css"
+  },
+  {
+    "revision": "29e81cbf049f0d6eb0c5",
+    "url": "/vk_rps/static/js/2.a191c4fd.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/vk_rps/static/js/2.a191c4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bbbfdbf292ea580c6a6",
+    "url": "/vk_rps/static/js/main.520430f7.chunk.js"
+  },
+  {
+    "revision": "bbdd585dfca384fb373c",
+    "url": "/vk_rps/static/js/runtime-main.7257728c.js"
+  }
+]);
