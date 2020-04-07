f31b49228707da3850b0874f2e5c6d48928b3823
xoma_star <40947642+xoma-star@users.noreply.github.com>
2020-04-07 20:16:10 +0500

    Updates

--- precache-manifest.17a498609d95936d1ca70790ac6f2763.js
+++ precache-manifest.17a498609d95936d1ca70790ac6f2763.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8329270a04e79d30213586b5b50d09ef",
+    "revision": "65e64b9c23c7cce4b81cabe303f5985a",
     "url": "/vk_rps/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk_rps/static/css/2.4fc5a6bc.chunk.css"
   },
   {
-    "revision": "b3e4ed2624b3b8f893ab",
+    "revision": "463eb08d08c62aad8790",
     "url": "/vk_rps/static/css/main.8447535d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/vk_rps/static/js/2.17b28064.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b3e4ed2624b3b8f893ab",
-    "url": "/vk_rps/static/js/main.05510879.chunk.js"
+    "revision": "463eb08d08c62aad8790",
+    "url": "/vk_rps/static/js/main.bcbe1686.chunk.js"
   },
   {
     "revision": "bbdd585dfca384fb373c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk_rps/precache-manifest.4f69aa9d0974ab265975ffab8e531865.js"
+  "/vk_rps/precache-manifest.17a498609d95936d1ca70790ac6f2763.js"
 );
 
 self.addEventListener('message', (event) => {
